323b856ec87920d813355448ce6d8137b8a61548
Eric <eric@Erics-MacBook-Air.local>
2026-02-05 14:58:11 +0300

    devController methods added

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
+use Faker\Factory as Faker;
 
 class DevController extends Controller
 {
@@ -25,7 +27,61 @@ class DevController extends Controller
 
         return null;
     }
+
     public function test()
     {
+    }
+
+    //создание рандомных проектов без использования factory seeder
+    public function addProject()
+    {
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 5; $i++) {
+            Project::create([
+                'name' => $faker->sentence(2),
+                'owner_id' => $faker->numberBetween(1, 6),
+                'is_active' => $faker->boolean(80),
+                'assignee_id' => $faker->numberBetween(1, 6),
+                'deadline_date' => $faker->dateTimeBetween('now', '+6 months'),
+            ]);
+        }
+    }
+
+    //получение всех проектов админов
+    public function getAdminProjects()
+    {
+        return Project::query()
+            ->whereHas('owner', function ($query) {
+                $query->where('role', 'admin');
+            })
+            ->with('owner')
+            ->get();
+    }
+
+    //проекты с просроченным дедлайном
+    public function getExpired()
+    {
+        return Project::query()
+            ->where('deadline_date', '<', now())
+            ->orderBy('deadline_date', 'asc')
+            ->get();
+    }
+
+    //берет рандомный проект и меняет на рандомные данные
+    public function updateRandom()
+    {
+        $faker = Faker::create();
+
+        return Project::query()
+            ->inRandomOrder()
+            ->firstOrFail()
+            ->update([
+                'name' => $faker->sentence(1),
+                'owner_id' => $faker->numberBetween(1, 6),
+                'is_active' => $faker->boolean(80),
+                'assignee_id' => $faker->numberBetween(1, 6),
+                'deadline_date' => $faker->dateTimeBetween('now', '+6 months'),
+            ]);
     }
 }
